0e1b534a83dba9f40b079209f9001d886c7d3f36
pfnazhmi <pfnazhmi@gmail.com>
2024-02-26 22:48:32 +0700

    add: hero konten

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero pt-40 pl-40 mt-5'>
+      <h1>Book is a window to the world</h1>
+      <p className='mb-10 mt-0'>wake up your dream by reading a book every day</p>
+      <a href="#" className='button-cta'>Book now</a>
+    </div>
   )
 }
 
